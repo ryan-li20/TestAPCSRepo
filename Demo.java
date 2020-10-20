4cc1d3919f0d49ba736a13610fe2627969faa61d
ryan-li20 <rli20@stuy.edu>
2020-10-20 14:06:57 -0400

    I tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -1,6 +1,14 @@
 public class Demo{
+  public static void print(int n){
+    for(int i = n; i > 0; i--){
+      for(int j = 0; j < i; j++){
+        System.out.print(n-i+1);
+      }
+      System.out.println();
+    }
+  }
   public static void main(String[] args){
     int arg = Integer.parseInt(args[0]);
-    printLoop.print(arg);
+    Demo.print(arg);
   }
 }
