b0950ca67f8ab23fc3396ac9067c8bf07d2fd3d3
ryan-li20 <rli20@stuy.edu>
2020-10-21 17:43:06 -0400

    Everything done for Java 10

--- Demo.java
+++ Demo.java
@@ -32,7 +32,7 @@ public class Demo{
 
   public static void main(String[] args){
     print(Integer.parseInt(args[0]));
-    //System.out.println(arrayDeepToString(create2DArray(4,4,4)));
+    //System.out.println(arrayDeepToString(create2DArrayRandomized(4,4,4)));
   }
 
   public static int[][] create2DArray(int rows, int cols, int maxValue){
@@ -46,5 +46,18 @@ public class Demo{
     return two;
   }
 
-  //public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue)
+  public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+    int[][] two = new int[rows][];
+    for(int i = 0; i < rows; i++){
+      int amt = (int)(Math.random()*(cols+1));
+      two[i] = new int[amt];
+    }
+    for(int i = 0; i < rows; i++){
+      for(int j = 0; j < two[i].length; j++){
+        int give = (int) (Math.random()*(maxValue+1));
+        two[i][j] = give;
+      }
+    }
+    return two;
+  }
 }
